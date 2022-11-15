ad6eeaf3f2ce2dadd154be88ba3bd0ba4512a9d6
Aitor C. Figueroa <aitorcf@gmail.com>
2022-11-15 09:54:25 +0100

    Ejercicio 2 update

--- src/com/ejercicios/Azar.java
+++ src/com/ejercicios/Azar.java
@@ -2,7 +2,7 @@ package com.ejercicios;
 
 public class Azar {
     public static int num(int min, int max) {
-        int random = (int) (5+(Math.random()*(max-min)));
+        int random = (int) (min+(Math.random()*(max-min)));
 
         return random;
     }
